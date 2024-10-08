76c4fb98868b7c7b5c0b0ec539dc2a63a45ff69e
Prashanth Vangari <vangariprashanth5298@gmail.com>
2024-10-07 20:55:04 -0500

    Initial commit

---
+++ Springboot-Kafka-Project/springboot-kafka-project/kafka-producer-wikimedia/src/main/java/com/prashanthvangari/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.prashanthvangari;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name("wikimedia_recentchange").build();
+    }
+}
